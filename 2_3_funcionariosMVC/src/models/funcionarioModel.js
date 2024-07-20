50c19d3cfba8892e02331c4985f2e721fac22380
Ronaldo Duarte <ronaldoduarteoliveirarodrigues@gmail.com>
2024-07-20 07:51:57 -0300

    :bug: fix: .env consertado para a forma correta e adicionado a criacao de tabelas de funcionarios

--- 2_3_funcionariosMVC/src/models/funcionarioModel.js
+++ 2_3_funcionariosMVC/src/models/funcionarioModel.js
@@ -2,9 +2,22 @@ import conn from '../config/conn.js'
 
 const tableFuncionarios = /*sql*/ `
     CREATE TABLE if NOT EXISTS funcionarios(
-
-
+        id varchar(60) primary key,
+        nome varchar(255) not null,
+        cargo varchar(255) not null,
+        data_contratacao date not null,
+        salario decimal(10,2) not null,
+        email varchar(255) not null,
         created_at timestamp default current_timestamp,
         updated_at timestamp default current_timestamp on update current_timestamp 
     )
 `
+
+conn.query(tableFuncionarios, (err, result, field)=>{
+    if(err){
+        console.error("Erro ao criar a tabela " +err.stack)
+        return
+    }
+
+    console.log("Tabela [funcionarios] criada com sucesso")
+})
